8c2c5347a72a9c2cf3447462fca90520473a6d29
Russel Arbore <russel.jma@gmail.com>
2026-02-05 10:54:31 -0800

    Assumptions object

--- implog/src/assumption.rs
+++ implog/src/assumption.rs
@@ -1,4 +1,5 @@
 use core::hash::Hash;
+use core::mem::take;
 use std::collections::BTreeSet;
 
 use crate::ast::Symbol;
@@ -22,7 +23,56 @@ impl DNFAssumption {
         }
     }
 
+    pub fn conjunct<I>(iter: I) -> Self
+    where
+        I: Iterator<Item = LeafAssumption>,
+    {
+        DNFAssumption {
+            dnf: BTreeSet::from([iter.collect()]),
+        }
+    }
+
     pub fn plus(&self, other: &Self) -> Self {
-        
+        let mut new = DNFAssumption {
+            dnf: self.dnf.union(&other.dnf).cloned().collect(),
+        };
+        new.weak_simplify();
+        new
+    }
+
+    pub fn times(&self, other: &Self) -> Self {
+        let mut new = DNFAssumption {
+            dnf: BTreeSet::new(),
+        };
+        for self_conj in &self.dnf {
+            for other_conj in &other.dnf {
+                new.dnf
+                    .insert(self_conj.union(other_conj).cloned().collect());
+            }
+        }
+        new.weak_simplify();
+        new
+    }
+
+    pub fn weak_simplify(&mut self) {
+        let mut to_remove = BTreeSet::new();
+        for conj1 in &self.dnf {
+            for conj2 in &self.dnf {
+                if conj1.is_superset(conj2) && conj1.len() != conj2.len() {
+                    to_remove.insert(conj1.clone());
+                }
+            }
+        }
+        self.dnf = self.dnf.difference(&to_remove).cloned().collect();
+    }
+
+    pub fn quotient(&mut self, leaf: LeafAssumption) {
+        self.dnf = take(&mut self.dnf)
+            .into_iter()
+            .map(|mut conj| {
+                conj.remove(&leaf);
+                conj
+            })
+            .collect();
     }
 }
